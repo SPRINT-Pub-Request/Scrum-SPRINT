68069c7964d7e51af023b5a9315bc5a37408f59e
Paolo <jplojo34@gmail.com>
2021-08-19 18:33:54 +0800

    manage users css

--- public/javascript/manageUsers.js
+++ public/javascript/manageUsers.js
@@ -11,4 +11,42 @@ $(document).ready(function () {
     $('#btnRemoveUser').click(function () {
         deleteUser($('#name'), $('#error'));
     });
+
+    $('#btn-edit').click(function(){
+        $('#committee').prop('disabled', false);
+        $('#role').prop('disabled', false);
+
+        $('#comm_Activities').prop('disabled', false);
+        $('#comm_Finance').prop('disabled', false);
+        $('#comm_HRD').prop('disabled', false);
+        $('#comm_Externals').prop('disabled', false);
+        $('#comm_TND').prop('disabled', false);
+        $('#comm_P-EVP').prop('disabled', false);
+        $('#comm_Secretariat').prop('disabled', false);
+        $('#comm_SocioCivic').prop('disabled', false);
+    });
+
+
+    var Modal = document.getElementById('userdetailsModal');
+    
+    Modal.addEventListener('show.bs.modal', function (event) {
+        //Modal on Open event handler
+        
+    })
+    
+    Modal.addEventListener('hidden.bs.modal', function (event) {
+        //Modal on Close event handler
+
+        $('#committee').prop('disabled', true);
+        $('#role').prop('disabled', true);
+
+        $('#comm_Activities').prop('disabled', true);
+        $('#comm_Finance').prop('disabled', true);
+        $('#comm_HRD').prop('disabled', true);
+        $('#comm_Externals').prop('disabled', true);
+        $('#comm_TND').prop('disabled', true);
+        $('#comm_P-EVP').prop('disabled', true);
+        $('#comm_Secretariat').prop('disabled', true);
+        $('#comm_SocioCivic').prop('disabled', true);
+    })
 });
