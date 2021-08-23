a5e99434f901c077d536e81ac9ee4ec11e3fe5a1
Jerickson <jerickson_lee@dlsu.edu.ph>
2021-08-23 17:06:08 +0800

    Selecting Sidebar should now be Highlighted

--- public/javascript/side.js
+++ public/javascript/side.js
@@ -3,5 +3,29 @@ $(document).ready(function () {
     $.get('/getEmail' , {} , function(result) {
         $('#email').text(result);
     });
-    
+
+    currLoc = $(location).attr('pathname');
+
+    if(currLoc == "/add_request") {
+        $('#add_request').addClass('nav-active')
+        $('#add_request#view_request').removeClass('nav-active');
+        $('#manage_users').removeClass('nav-active');
+        $('#manage_request').removeClass('nav-active');
+    } else if(currLoc == "/view_request") {
+        $('#view_request').addClass('nav-active')
+        $('#add_request').removeClass('nav-active');
+        $('#manage_users').removeClass('nav-active');
+        $('#manage_request').removeClass('nav-active');
+    } else if(currLoc == "/manage_users") {
+        $('#manage_users').addClass('nav-active')
+        $('#view_request').removeClass('nav-active');
+        $('#add_request').removeClass('nav-active');
+        $('#manage_request').removeClass('nav-active');
+    } else if(currLoc == "/manage_request") {
+        $('#manage_request').addClass('nav-active')
+        $('#view_request').removeClass('nav-active');
+        $('#manage_users').removeClass('nav-active');
+        $('#add_request').removeClass('nav-active');
+    }
+
 });
