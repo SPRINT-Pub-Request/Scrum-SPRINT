06af7c1f4c28d8d18e43b0293ab171688da3c5f1
Jerickson <jerickson_lee@dlsu.edu.ph>
2021-09-08 21:59:02 +0800

    Minor Changes

--- public/javascript/manageRequest.js
+++ public/javascript/manageRequest.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     $.get('/getRole' , {} , function(result) {
         if(result === "Secretariat") 
-            $('.bi-trash-fill').remove();
+            $('.bi-trash-fill').css("visibility" , "hidden");
     });
 
 
