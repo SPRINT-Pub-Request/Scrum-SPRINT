958225bfadd2cf774f5cda9d4da496cf652f81d0
Jerickson <jerickson_lee@dlsu.edu.ph>
2021-09-19 00:03:33 +0800

    forgot to push this

--- controllers/viewRequestController.js
+++ controllers/viewRequestController.js
@@ -168,6 +168,7 @@ const viewRequestController = {
         changes = {
             pubLink : req.query.pubLink
         }
+
         db.updateOne(PubRequest, {request_id: req.query.request_id}, changes, function(result){
             res.send(result);
         });
